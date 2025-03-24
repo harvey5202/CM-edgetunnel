840ec1a5d337abb766ffbdf6473006ef8185ff19
harvey5202 <204728521+harvey5202@users.noreply.github.com>
2025-03-24 22:02:24 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = '16642731-73af-4d04-8a41-2262cc95d795';
 let proxyIP = '';
 //let sub = '';
 let subConverter = atob('U1VCQVBJLkNNTGl1c3Nzcy5uZXQ=');
